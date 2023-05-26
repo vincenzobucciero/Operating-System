2507387f6a6f523976c704dcc3c6be2c0d87e483
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-05-26 16:42:53 +0200

    w comment

--- Thread/esercizio1.c
+++ Thread/esercizio1.c
@@ -14,33 +14,40 @@
 #include <pthread.h>
 #include <unistd.h>
 
+//variabile globale da incrementare
 int var_glob = 0;
+//mutex per l'accesso sincronizzato alla variabile globare condivisa
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *routine() {
+    // Genera un numero casuale tra 1 e 2 per la sleep
     int n = rand() % 2 + 1;
     printf("Thread %ld sleeping for %d seconds\n", pthread_self(), n);
     sleep(n);
 
+    //Acquisisce il lock del mutex per proteggere l'accesso alla variabile globale
     pthread_mutex_lock(&mutex);
+    //Incremento la variabile
     var_glob++;
+    //Sblocco il lock del mutex
     pthread_mutex_unlock(&mutex);
 
+    //Termina il thread
     pthread_exit(NULL);
 }
 
 int main() {
     int n, rc;
 
+    //Legge il numero di thread da creare dall'input
     printf("Inserisci numero di thread:  ");
     scanf(" %d", &n);
 
+    //Alloca spazio per gli ID dei thread
     pthread_t *tid = malloc(n*sizeof(pthread_t));
-    // int **taskids = malloc(n*sizeof(int*));
 
     for(int i = 0; i < n; i++) {
-        //taskids[i] = malloc(sizeof(int));
-        //*taskids[i] = i;
+        //Crea thread
         printf("Creating thread %d\n", i);
         rc = pthread_create(&tid[i], NULL, routine, NULL);
 
@@ -52,13 +59,16 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
+        //Aspetta la terminazione di ciascun thread
         rc = pthread_join(tid[i], NULL);
+
         if (rc) {
             printf("ERROR, return code from pthread_join() is %d\n", rc);
             exit(-1);
         }
     }
 
+    //Stampa il valore finale della variabile globale
     printf("Variabile globale:  %d\n", var_glob);
 
     return 0;
