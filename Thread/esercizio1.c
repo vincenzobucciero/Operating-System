d2b87d0a7c3aaba3eaefe00d48ba139a76fbf2b1
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-05-27 20:42:22 +0200

    ok  mod es

--- Thread/esercizio1.c
+++ Thread/esercizio1.c
@@ -16,27 +16,28 @@
 
 //variabile globale da incrementare
 int var_glob = 0;
-//mutex per l'accesso sincronizzato alla variabile globare condivisa
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *routine() {
-    // Genera un numero casuale tra 1 e 2 per la sleep
-    int n = rand() % 2 + 1;
-    printf("Thread %ld sleeping for %d seconds\n", pthread_self(), n);
+void *routine(void *arg) {
+    //Variabile per l'identificazione del thread
+    int threadId = *((int*)arg);
+
+    //Genera un numero casuale tra 1 e 2 per la sleep
+    int n = rand() % 10 + 1;
+    
+    printf("Thread %d -> var glob %d\n", threadId, threadId);
+    printf("Thread %d sleep for %d second(s)\n", threadId, n);
+
     sleep(n);
 
-    //Acquisisce il lock del mutex per proteggere l'accesso alla variabile globale
-    pthread_mutex_lock(&mutex);
     //Incremento la variabile
     var_glob++;
-    //Sblocco il lock del mutex
-    pthread_mutex_unlock(&mutex);
 
     //Termina il thread
     pthread_exit(NULL);
 }
 
 int main() {
+    /*
     int n, rc;
 
     //Legge il numero di thread da creare dall'input
@@ -70,6 +71,35 @@ int main() {
 
     //Stampa il valore finale della variabile globale
     printf("Variabile globale:  %d\n", var_glob);
+
+    return 0;
+    */
+
+    int n;
+
+    //Legge il numero di thread da creare dall'input
+    printf("Inserisci numero di thread:  ");
+    scanf(" %d", &n);
+
+    pthread_t* threads = (pthread_t*)malloc(n * sizeof(pthread_t));
+
+    int* threadIds = (int*)malloc(n * sizeof(int));
+
+    // Crea n thread
+    for (int i = 0; i < n; i++) {
+        threadIds[i] = i + 1;
+        pthread_create(&threads[i], NULL, routine, &threadIds[i]);
+    }
+
+    // Attendi la terminazione dei thread
+    for (int i = 0; i < n; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Variabile globale totale: %d\n", var_glob);
+
+    free(threads);
+    free(threadIds);
 
     return 0;
 
