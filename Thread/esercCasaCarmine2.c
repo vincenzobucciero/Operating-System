a28d14785f628362eb4eaf2ba37f366307d95ee8
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-06-11 12:42:23 +0200

    comm so fort

--- Thread/esercCasaCarmine2.c
+++ Thread/esercCasaCarmine2.c
@@ -12,52 +12,100 @@
 #include <unistd.h>
 #include <pthread.h>
 
-int n;
-int *vettore;
-float media;
+typedef struct {
+    int *vettore;
+    int n;
+    int threadID;
+    int sommaLocale;
+    int count;
+    float media;
+} sharedData;
 
 pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *routine(void *arg){
-    int id = *(int*)arg;
-    printf("Thread ID: %d\n", id);
+void allocaVettore(sharedData *data){
+    //Alloco vettore
+    data->vettore = (int*)calloc(data->n, sizeof(int));
+    //Riempio vettore
+    srand(time(NULL));
+    for(int i = 0; i < data->n; i++){
+        data->vettore[i] = rand() % 10;
+    }
+    //Stampo vettore
+    for(int i = 0; i < data->n; i++){
+        printf("%d  ", data->vettore[i]);
+    }
+    printf("\n");
+}
 
-    int inizio = id * (n/4);
-    int fine = inizio + (n/4);
+void *routine(void *arg){
+    sharedData *data = (sharedData*)arg;
+
+    int inizio = data->threadID * (data->n / 4);
+    int fine = inizio + (data->n / 4);
+    int s = 0;
+    int numElementiMinori = 0;
 
     for(int i = inizio; i < fine; i++){
-        
+        s += data->vettore[i];
     }
+
+    data->sommaLocale += s;
+
+    data->media = (float)data->sommaLocale / (data->n /4);
+
+    for(int i = inizio; i < fine; i++){
+        if(data->vettore[i] < data->media){
+            numElementiMinori++;
+        }
+    }
+
+    pthread_mutex_lock(&myMutex);
+    data->count += numElementiMinori;
+    pthread_mutex_unlock(&myMutex);
+
+    printf("Thread %d => SottoVettore:  %d => Somma: %d => Media:  %f\n", data->threadID, data->threadID + 1, data->sommaLocale, data->media);
+    printf("\t\tSottoVettore:  %d => Numero elementi minori della media:  %d\n", data->threadID + 1, data->count);
+
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
-    if(argc < 2){
-        perror("Errore\n");
+    sharedData data;
+    if (argc < 2) {
+        printf("Inserire il numero di elementi del vettore\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    data.n = atoi(argv[1]);
+
+    if(data.n < 24 || data.n%4 != 0){
+        printf("Il numero di elementi deve essere n>=24 e un multiplo di 4\n");
         exit(EXIT_FAILURE);
     }
 
-    n = atoi(argv[1]);
+    allocaVettore(&data);
 
-    if(n < 24 || n%4 != 0){
-        perror("Devi inserisce un numero n>=24 e multiplo di 4..\n");
-        exit(EXIT_FAILURE);
+    pthread_t threads[4];
+    sharedData thread_data[4];
+
+    for(int i = 0; i < 4; i++){
+        thread_data[i].vettore = data.vettore;
+        thread_data[i].n = data.n;
+        thread_data[i].threadID = i;
+
+        if(pthread_create(&threads[i], NULL, routine, (void*)&thread_data[i]) != 0){
+            perror("Errore creazione thread\n");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    vettore = (int*)malloc(n*sizeof(int));
-
-    srand(time(NULL));
-    for(int i = 0; i < n; i++){
-        vettore[i] = rand()%10;
+    for(int i = 0; i < 4; i++){
+        if(pthread_join(threads[i], NULL) != 0){
+            perror("Errore join thread\n");
+            exit(EXIT_FAILURE);
+        }
     }
-
-    for(int i = 0; i < n; i++){
-        printf("%d  ", vettore[i]);
-    }
-
-    printf("\n");
-
-    media = 
-
 
     return 0;
 }
